dcd82886763f1f23b14bd3b8e74e177b54f81333
oboehm <o.boehm@optica.de>
2018-07-08 18:45:43 +0200

    generated send-document request is compared against reference request

--- src/test/java/org/cups4j/operations/ipp/IppSendDocumentOperationTest.java
+++ src/test/java/org/cups4j/operations/ipp/IppSendDocumentOperationTest.java
@@ -2,14 +2,17 @@ package org.cups4j.operations.ipp;
 
 import ch.ethz.vppserver.ippclient.IppResponse;
 import ch.ethz.vppserver.ippclient.IppResult;
+import org.apache.commons.io.FileUtils;
 import org.cups4j.CupsPrinter;
 import org.cups4j.CupsPrinterTest;
+import org.cups4j.ipp.attributes.Attribute;
 import org.cups4j.ipp.attributes.AttributeGroup;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
@@ -19,11 +22,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * Unit-Tests fuer {@link IppSendDocumentOperation}-Klasse.
@@ -51,10 +53,10 @@ public class IppSendDocumentOperationTest extends AbstractIppOperationTest {
      * For the send-document command it is important, that the header contains
      * the job-id. This is tested here.
      *
-     * @throws UnsupportedEncodingException in case of encoding problemss
+     * @throws IOException in case of encoding or other problemss
      */
     @Test
-    public void testGetIppHeaderWithJobId() throws UnsupportedEncodingException {
+    public void testGetIppHeaderWithJobId() throws IOException {
         URL printerURL = createURL("http://localhost:631/test-printer");
         Map<String, String> attributes = setUpAttributes();
         attributes.put("operation-attributes", "job-id:integer:40#last-document:boolean:false");
@@ -62,20 +64,38 @@ public class IppSendDocumentOperationTest extends AbstractIppOperationTest {
         byte[] header = toByteArray(buffer);
         assertThat(new String(header), containsString("job-id"));
         checkIppRequest(header);
+        checkIppRequestAttributes(header);
     }
 
-    private static void checkIppRequest(byte[] header) {
+    private static void checkIppRequest(byte[] header) throws IOException {
+        IppResult ippResult = new IppResponse().getResponse(ByteBuffer.wrap(header));
+        Set<String> groupTagNames = new HashSet<String>();
+        for (AttributeGroup group : ippResult.getAttributeGroupList()) {
+            String tagName = group.getTagName();
+            assertThat("duplicate tag name", groupTagNames, not(hasItem(tagName)));
+            groupTagNames.add(tagName);
+        }
+    }
+
+    private static void checkIppRequestAttributes(byte[] header) throws IOException {
         IppResponse ippResponse = new IppResponse();
-        try {
-            IppResult ippResult = ippResponse.getResponse(ByteBuffer.wrap(header));
-            Set<String> groupTagNames = new HashSet<String>();
-            for (AttributeGroup group : ippResult.getAttributeGroupList()) {
-                String tagName = group.getTagName();
-                assertThat("duplicate tag name", groupTagNames, not(hasItem(tagName)));
-                groupTagNames.add(tagName);
+        IppResult ippResult = ippResponse.getResponse(ByteBuffer.wrap(header));
+        IppResult ref = ippResponse.getResponse(
+                ByteBuffer.wrap(FileUtils.readFileToByteArray(new File("src/test/resources/ipp/Send-Document.ipp"))));
+        for (AttributeGroup group : ref.getAttributeGroupList()) {
+            checkAttributeGroupList(group, ippResult.getAttributeGroup(group.getTagName()));
+        }
+    }
+
+    private static void checkAttributeGroupList(AttributeGroup ref, AttributeGroup attributeGroup) {
+        Set<String> attributeNames = new HashSet<String>();
+        for (Attribute attr : attributeGroup.getAttribute()) {
+            attributeNames.add(attr.getName());
+        }
+        for (Attribute attr : ref.getAttribute()) {
+            if (!attributeNames.contains(attr.getName())) {
+                fail("attribute '" + attr.getName() + "' is missing in " + attributeGroup);
             }
-        } catch (IOException ex) {
-            throw new IllegalArgumentException("cannot parse header with " + header.length + " bytes", ex);
         }
     }
 
